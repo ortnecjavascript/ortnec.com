e820f396bdd294679fd26fd7c571f79b3f5d50e8
d.filipovs <d.filipovs@cardpay.com>
2014-12-23 08:45:20 +0200

    Simple start

---
+++ src/main/java/test/ElementManagerTest.js
@@ -0,0 +1,5 @@
+test("simple test", function(){
+    var value = doChanges('label',MetaData.FIRST_EVENT.quest);
+    assert.ok(null !== value);
+});
+//for some reason test not working.... permanently ((
